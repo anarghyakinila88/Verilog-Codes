aa8bde9289d1f2c232585c9fc64a246d4bbb180a
Anarghya Kinila <88137286+anarghyakinila88@users.noreply.github.com>
2023-03-19 15:06:59 +0530

    Create mod_12_counter.v

---
+++ MOD12 counter/mod_12_counter.v
@@ -0,0 +1,19 @@
+module mod_12_counter(clk,rst,load,data,count);
+input clk,rst,load;
+input [3:0] data;
+output reg [3:0] count;
+
+always@(posedge clk)
+begin
+if(rst)
+count<=4'd0;
+else if (load && data < 12)
+count<=data;
+else if(count==4'd11)
+count<=4'd0;
+else
+count<=count+1'b1;
+end
+
+
+endmodule
